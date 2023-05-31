21794dacacc39d5c8d7a89912ab9409874ca4077
Lodhi, Vikram <vikram.lodhi@barco.com>
2023-05-31 21:26:24 +0530

    Static removed from Greeks

--- Greeks/Greeks.hpp
+++ Greeks/Greeks.hpp
@@ -4,21 +4,21 @@
 #include <cstdint>
 
 namespace Greeks {
-static double GetDelta(double s, double k, double v, double r, double t, bool IsCall);
+double GetDelta(double s, double k, double v, double r, double t, bool IsCall);
 
-static double GetGamma(double s, double k, double v, double r, double t, bool IsCall);
+double GetGamma(double s, double k, double v, double r, double t, bool IsCall);
 
-static double GetVega(double s, double k, double v, double r, double t, bool IsCall);
+double GetVega(double s, double k, double v, double r, double t, bool IsCall);
 
-static double GetRho(double s, double k, double v, double r, double t, bool IsCall);
+double GetRho(double s, double k, double v, double r, double t, bool IsCall);
 
-static double GetTheta(double s, double k, double v, double r, double t, bool IsCall);
+double GetTheta(double s, double k, double v, double r, double t, bool IsCall);
 
-static double GetIV(double s, double k, double r, double t, double p, bool IsCall);
+double GetIV(double s, double k, double r, double t, double p, bool IsCall);
 
-static double GetOptionPrice(double s, double k, double v, double r, double t, bool IsCall);
+double GetOptionPrice(double s, double k, double v, double r, double t, bool IsCall);
 
-static double GetExpiryGap(uint32_t Time);
+double GetExpiryGap(uint32_t Time);
 
 }  // namespace Greeks
 
